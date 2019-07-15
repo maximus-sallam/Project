6cd90c8228f9168101a8a87e73c6ee558615d3f2
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-15 18:43:56 +0800

    created basic calculator

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3.7
+
+while True:
+    print("Options:")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to subtract two numbers")
+    print("Enter 'divide' to subtract two numbers")
+    print("Enter 'quit' to quit the program")
+
+    user_input = input(": ")
+
+    if user_input == "quit":
+        break
+    elif user_input == "add":
+
+    elif user_input == "subtract":
+
+    elif user_input == "multiply":
+
+    elif user_input == "divide":
+
+    else:
+        print("Unknown input")
