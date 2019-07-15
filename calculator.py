1bb6f1c93d1a9e8ea2ff6acc741b7bd2ef144c22
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-15 18:53:29 +0800

    completed calculator.py

--- calculator.py
+++ calculator.py
@@ -12,13 +12,30 @@ while True:
 
     if user_input == "quit":
         break
+
     elif user_input == "add":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 + num2)
+        print("The answer is " + result)
 
     elif user_input == "subtract":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 - num2)
+        print("The answer is " + result)
 
     elif user_input == "multiply":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 * num2)
+        print("The answer is " + result)
 
     elif user_input == "divide":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 / num2)
+        print("The answer is " + result)
 
     else:
         print("Unknown input")
