3fa502d7cc889f40bce61b3611d68cbe970da4b7
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-15 19:03:54 +0800

    added exception handling to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,41 +1,48 @@
 #!/usr/local/bin/python3.7
 
 while True:
-    print("Options:")
-    print("Enter 'add' to add two numbers")
-    print("Enter 'subtract' to subtract two numbers")
-    print("Enter 'multiply' to subtract two numbers")
-    print("Enter 'divide' to subtract two numbers")
-    print("Enter 'quit' to quit the program")
+    try:
+        print("Options:")
+        print("Enter 'add' to add two numbers")
+        print("Enter 'subtract' to subtract two numbers")
+        print("Enter 'multiply' to subtract two numbers")
+        print("Enter 'divide' to subtract two numbers")
+        print("Enter 'quit' to quit the program")
 
-    user_input = input(": ")
+        user_input = input(": ")
 
-    if user_input == "quit":
-        break
+        if user_input == "quit":
+            break
 
-    elif user_input == "add":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 + num2)
-        print("The answer is " + result)
+        elif user_input == "add":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = str(num1 + num2)
+            print("The answer is " + result)
 
-    elif user_input == "subtract":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 - num2)
-        print("The answer is " + result)
+        elif user_input == "subtract":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = str(num1 - num2)
+            print("The answer is " + result)
 
-    elif user_input == "multiply":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 * num2)
-        print("The answer is " + result)
+        elif user_input == "multiply":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = str(num1 * num2)
+            print("The answer is " + result)
 
-    elif user_input == "divide":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 / num2)
-        print("The answer is " + result)
+        elif user_input == "divide":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = str(num1 / num2)
+            print("The answer is " + result)
 
-    else:
-        print("Unknown input")
+        else:
+            print("Unknown input")
+
+    except:
+        print("Are you trying to kill me?")
+
+    finally:
+        print("You big dummy!")
