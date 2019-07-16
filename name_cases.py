8e06ea56cf31550fbd0e58e85628526059a26db6
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-16 19:14:00 +0800

    added work to name_cases.py

--- name_cases.py
+++ name_cases.py
@@ -3,3 +3,10 @@
 name = "Maximus"
 message = "Hello " + name + ", would you like to learn some Python today?"
 print(message)
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = ' once said, "Bitch, quit playin\'."'
+print(name + quote)
