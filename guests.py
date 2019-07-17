82337fab4ac82c94c5fbf272aac9f630cb8d1087
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-17 20:09:11 +0800

    improved the code for guests.py and added comments

--- guests.py
+++ guests.py
@@ -1,20 +1,41 @@
 #!/usr/local/bin/python3.7
-guests = [ 'Albert Einstein', 'Thomas Edison', 'Nikola Tesla', 'Elon Musk' ]
+guests = [ 'Albert Einstein', 'Thomas Edison', 'Nikola Tesla', 'Elon Musk' ] # This is my list of guests
 
-print("Please come over and eat my food, " + guests[0])
-print("Please come over and eat my food, " + guests[1])
-print("Please come over and eat my food, " + guests[2])
-print("Please come over and eat my food, " + guests[3])
+num = len(guests) # This is how many guests are in the list
+i = 0
+while i < num: #This itterates through each guest and prints the guests name with a message
+    print("Please come over and eat my food, " + guests[i])
+    i = i + 1
+
+print("") #This is white space
 
 print("Oh dear! It appears that " + guests[2] + " won't be joining us.")
 
-cant = guests.pop(2)
-guests.insert(2, 'Alexander Shulgin')
+cant = guests.pop(2) # This removes Nikola Tesla from the list and stores it as cant
+guests.insert(2, 'Alexander Shulgin') # Inserts guest into the 2 position
 
-print("Please come over and eat my food, " + guests[0])
-print("Please come over and eat my food, " + guests[1])
-print("Please come over and eat my food, " + guests[2])
-print("Please come over and eat my food, " + guests[3])
+print("") # This is white space
 
+i = 0
+while i < num: # This itterates through each guest and prints the guests name with a message
+    print(guests[i] + ", I've found a bigger table!")
+    i = i + 1
 
+print("") # This is white space
 
+guests.insert(0, 'Richard Feynman') # Adds guest to the beginning of the list
+guests.insert(3, 'Pablo Escobar') # Inserts guest into the 3 position
+guests.append('Freddy Krueger') # Appends guest to the end of the list
+num = len(guests)
+
+i = 0
+while i < num: # This itterates through each guest and prints the guests name with a message
+    print(guests[i] + ", I've found a bigger table!")
+    i = i + 1
+
+print("") # This is white space
+
+i = 0
+while i < num: # This itterates through each guest and prints the guests name with a message
+    print("Please come over and eat my food, " + guests[i])
+    i = i + 1
