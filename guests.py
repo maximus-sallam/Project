c456f0d2dc82cadd501a7e412c60e60967a2dcb3
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-17 19:08:12 +0800

    added guests.py inviting my favorite people to dinner

---
+++ guests.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.7
+guests = [ 'Albert Einstein', 'Thomas Edison', 'Nikola Tesla', 'Elon Musk' ]
+
+print("Please come over and eat my food, " + guests[0])
+print("Please come over and eat my food, " + guests[1])
+print("Please come over and eat my food, " + guests[2])
+print("Please come over and eat my food, " + guests[3])
+
+
