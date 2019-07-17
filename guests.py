8a9875d308e47dda0fe9fc92e36cfda1030cf5df
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-17 20:34:57 +0800

    completed the task on page 47

--- guests.py
+++ guests.py
@@ -3,17 +3,15 @@ guests = [ 'Albert Einstein', 'Thomas Edison', 'Nikola Tesla', 'Elon Musk' ] # T
 
 num = len(guests) # This is how many guests are in the list
 i = 0
-while i < num: #This itterates through each guest and prints the guests name with a message
+while i < num: # This itterates through each guest and prints the guests name with a message
     print("Please come over and eat my food, " + guests[i])
     i = i + 1
 
-print("") #This is white space
-
+print("") # This is white space
 print("Oh dear! It appears that " + guests[2] + " won't be joining us.")
 
 cant = guests.pop(2) # This removes Nikola Tesla from the list and stores it as cant
 guests.insert(2, 'Alexander Shulgin') # Inserts guest into the 2 position
-
 print("") # This is white space
 
 i = 0
@@ -25,7 +23,6 @@ guests.insert(0, 'Richard Feynman') # Adds guest to the beginning of the list
 guests.insert(3, 'Pablo Escobar') # Inserts guest into the 3 position
 guests.append('Freddy Krueger') # Appends guest to the end of the list
 num = len(guests)
-
 print("") # This is white space
 
 i = 0
@@ -33,4 +30,28 @@ while i < num: # This itterates through each guest and prints the guests name wi
     print("Please come over and eat my food, " + guests[i])
     i = i + 1
 
+print("") # This is white space
+print("Oh dear! The Hulk smashed the table and now I can only invite two people")
 
+print("")
+sorry = guests.pop()
+print("Sorry " + sorry + ". You're no longer invited")
+sorry = guests.pop()
+print("Sorry " + sorry + ". You're no longer invited")
+sorry = guests.pop()
+print("Sorry " + sorry + ". You're no longer invited")
+sorry = guests.pop()
+print("Sorry " + sorry + ". You're no longer invited")
+sorry = guests.pop()
+print("Sorry " + sorry + ". You're no longer invited")
+num = len(guests)
+print("")
+
+i = 0
+while i < num: #
+    print("I hope to see you at dinner, " + guests[i])
+    i = i + 1
+
+del guests[0]
+del guests[0]
+print(guests)
