212bff011c7cb567f75197f03930d9eea214ba44
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-19 14:27:50 +0800

    added pizza.py

---
+++ pizza.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3.7
+pizzas = ['cheese','pepperoni','meat','garlic']
+for pizza in pizzas:
+    print("Would you like a " + pizza.title() + " Pizza?\n")
+print("There are so many different pizzas to choose from here!")
+print("Go ahead... Stick the pizza in your butt.")
+print("That's it. Who's a dirty pizza lover? You are!")
+
+
+
+
+
