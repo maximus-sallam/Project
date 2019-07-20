a75117730282aa1ca6165e8ef968cc0e0f9d61c3
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 10:15:37 +0800

    added code to foods.py

--- foods.py
+++ foods.py
@@ -3,8 +3,14 @@
 my_foods = ['pizza','burger','cake','pie','tacos']
 friend_foods = my_foods[:]
 
+my_foods.append('pancakes')
+friend_foods.append('turds')
+
 print("My favorite foods are:")
 print(my_foods)
 
 print("\nMy friend's favorite foods are:")
 print(friend_foods)
+
+
+
