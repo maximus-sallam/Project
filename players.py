a88da3e9bae2158b69cd7c24b78af659560e7904
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 09:54:48 +0800

    working on chapter 4

---
+++ players.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.7
+players = ['charles','martina','micheal','florence','eli']
+
+print("Here are the first three players on my team:\n")
+for player in players[:3]:
+    print(player.title())
+print("\nGiggity")
