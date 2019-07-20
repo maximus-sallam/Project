84c36078871b168b99a8e1250b0281b4f38ace70
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 10:52:39 +0800

    added my true favorite pizzas

--- pizza.py
+++ pizza.py
@@ -1,12 +1,17 @@
 #!/usr/local/bin/python3.7
 pizzas = ['cheese','pepperoni','meat','garlic']
+friend_pizzas = pizzas[:]
+
 for pizza in pizzas:
     print("Would you like a " + pizza.title() + " Pizza?\n")
+
 print("There are so many different pizzas to choose from here!")
 print("Go ahead... Stick the pizza in your butt.")
-print("That's it. Who's a dirty pizza lover? You are!")
+print("That's it. Who's a dirty pizza lover? You are!\n")
 
+pizzas.append('angeles pokpok')
+friend_pizzas.append('bangkok pokpok')
 
-
-
+print(pizzas)
+print(friend_pizzas)
 
