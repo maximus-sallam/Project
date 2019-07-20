4a89589744a16fb77f1b3fe83775e34490e7f625
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 08:40:35 +0800

    completed 4-1 through 4-9

--- num.py
+++ num.py
@@ -3,6 +3,38 @@ num = 69
 message = "My favorite number is " + str(num) + "!"
 print(message)
 
-numbers = list(range(0,999))
-for number in numbers:
-    print(number)
+#numbers = list(range(0,999))
+#for number in numbers:
+#    print(number)
+
+squares = []
+for value in range(1,11):
+#    square = value ** 2
+#    squares.append(square)
+    squares.append(value ** 2)
+print(squares)
+
+purple = [digit**2 for digit in range(1,11)]
+print(purple)
+
+purple = [digit**2 for digit in range(1,11)]
+print(purple)
+
+totwenty = list(range(1,21,2))
+for twenty in totwenty:
+    print(twenty)
+
+million = list(range(1,1000001))
+#for mil in million:
+#    print(mil)
+print(min(million))
+print(max(million))
+print(sum(million))
+
+cubes_two = []
+for cube_two in range(1,11):
+    cubes_two.append(cube_two ** 3)
+print(cubes_two)
+
+cubes = [cube**3 for cube in range(1,11)]
+print(cubes)
--- pypets.py
+++ pypets.py
@@ -1,100 +1,175 @@
 #!/usr/local/bin/python3.7
-## Set availabel pet types and actions
-petTypes = ('dog', 'cat', 'rabbit', 'lizzard', 'bird', 'fish')
-petActions = ('feed', 'walk', 'play', 'sleep', 'nap')
+## Set available pet types and actions
+petTypes = ('dog', 'cat', 'rabbit', 'lizard', 'bird', 'fish','unicorn')
+petActions = ('feed', 'walk', 'play', 'sleep', 'nap','kill')
 
-## Get player name and pet type
-playerName = input("Welcome to pypets! \nWhat is your name? ").title()
-print("what kind of pet would you like?")
+## Pet class
+class Pet():
+    """Pet class"""
+    def __init__(self, name, owner):
+        self.name = name
+        self.owner = owner
+        self.heath = 100
+        self.happy = 100
+        self.rest = 100
+        self.hunger = 0
+
+    def get_health(self):
+        return self.health
+
+    def get_happy(self):
+        return self.happy
+
+    def get_rest(self):
+        return self.rest
+
+    def get_hunger(self):
+        return self.hunger
+
+    def update_health(self, health):
+        self.health = health
+
+    def update_happy(self, happy):
+        self.happy = happy
+
+    def update_rest(self, rest):
+        self.rest = rest
+
+    def update_hunger(self, hunger):
+        self.hunger = hunger
+
+    def feed(self):
+        """Pet eats"""
+        print(self.name + " eats what you gave them.")
+
+    def walk(self):
+        """Walk pet"""
+        print("You take " + self.name + " for a walk.")
+
+    def play(self):
+        """Pet plays"""
+        print("You play with" + self.name + ".")
+
+    def sleep(self):
+        """Pet sleeps"""
+        print(self.name + " sleeps.")
+
+    def nap(self):
+        """Pet naps"""
+        print(self.name + " takes a nap.")
+
+    def kill(self):
+        """Pet dies"""
+        print("You murdered " + self.name + ". You monster!")
+
+## End of pet class
+## Species Classes
+class Dog(Pet):
+	"""Dog class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attributes of the Pet class"""
+		super().__init__(name, owner)
+
+
+class Cat(Pet):
+	"""Cat class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attibutes of the Pet class"""
+		super().__init__(name, owner)
+
+class Rabbit(Pet):
+	"""Rabbit class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attibutes of the Pet class"""
+		super().__init__(name, owner)
+
+class Lizard(Pet):
+	"""Lizard class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attibutes of the Pet class"""
+		super().__init__(name, owner)
+
+class Bird(Pet):
+	"""Bird class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attibutes of the Pet class"""
+		super().__init__(name, owner)
+
+class Fish(Pet):
+	"""Fish class"""
+
+	def __init__(self, name, owner):
+		"""Initialize attibutes of the Pet class"""
+		super().__init__(name, owner)
+
+class Unicorn(Pet):
+    """Unicorn class"""
+
+    def __init__(self, name, owner):
+        """Initialize attibutes of the Pet class"""
+        super().__init__(name, owner)
+
+##End of species classes
+
+## Get player name
+owner = input("Welcome to pypets! \nWhat is your name?\n\t").title()
+print("what kind of pet would you like?\n\t")
 typeMenuNumber = 1
 for types in petTypes:
-    print(str(typeMenuNumber) + " " + types)
-    typeMenuNumber += 1
+	print(str(typeMenuNumber) + " " + types)
+	typeMenuNumber += 1
 
 typeChoice = int(input("number: "))
 petType = petTypes[typeChoice-1]
 
-## Pet Classes
-class Dog():
-    """Dog class"""
-
-    def __init__(self, name, owner):
-        """Initialize name and age attributes"""
-        self.name = name
-        self.owner = owner
-
-    def feed(self):
-        """Dog eats"""
-        print(self.name + " eats what you gave them.")
-
-    def walk(self):
-        """Walk dog"""
-        print("You take " + self.name + " for a walk.")
-
-    def play(self):
-        """Dog plays"""
-        print("You play with" + self.name + ".")
-
-    def sleep(self):
-        """Dog sleeps"""
-        print(self.name + " sleeps.")
-
-    def nap(self):
-        """Dog naps"""
-        print(self.name + " takes a nap.")
-
-class Cat():
-    """Cat class"""
-
-    def __init__(self, name, owner):
-        """Initialize name and age attributes"""
-        self.name = name
-        self.owner = owner
-
-    def feed(self):
-        """Cat eats"""
-        print(self.name + " eats what you gave them.")
-
-    def walk(self):
-        """Walk Cat"""
-        print(self.name + " doesn't what to go for a walk. it's a cat, dummy")
-
-    def play(self):
-        """Cat plays"""
-        print("You play with" + self.name + ".")
-
-    def sleep(self):
-        """Cat sleeps"""
-        print(self.name + " sleeps.")
-
-    def nap(self):
-        """Cat naps"""
-        print(self.name + " takes a nap.")
-
-##End of pet classes
+name = input("What would you like to name your " + petType +"?\n\t")
 
 ## Sorry, alpha
 if typeChoice == 1:
-    petName = input("What would you like to name your " + petType +"?")
-    player_dog = Dog(petName, playerName)
-    print("\n\nOk, " + player_dog.owner + " your new " + petType + " is named " + player_dog.name + "!")
-    print("\n\nWhat would you like to do with " + player_dog.name + "?")
-    currentPet = player_dog
+	player_dog = Dog(name, owner)
+	print("\n\nOk, " + player_dog.owner + " your new " + petType + " is named " + player_dog.name + "!")
+	currentPet = player_dog
 elif typeChoice == 2:
-    petName = input("What would you like to name your " + petType +"?")
-    player_cat = Cat(petName, playerName)
-    print("\n\nOk, " + player_cat.owner + " your new " + petType + " is named " + player_cat.name + "!")
-    print("\n\nWhat would you like to do with " + player_cat.name + "?")
-    currentPet = player_cat
+	player_cat = Cat(name, owner)
+	print("\n\nOk, " + player_cat.owner + " your new " + petType + " is named " + player_cat.name + "!")
+	currentPet = player_cat
+elif typeChoice == 3:
+	player_rabbit = Rabbit(name, owner)
+	print("\n\nOk, " + player_rabbit.owner + " your new " + petType + " is named " + player_rabbit.name + "!")
+	currentPet = player_rabbit
+elif typeChoice == 4:
+	player_lizard = Lizard(name, owner)
+	print("\n\nOk, " + player_lizard.owner + " your new " + petType + " is named " + player_lizard.name + "!")
+	currentPet = player_lizard
+elif typeChoice == 5:
+	player_bird = Bird(name, owner)
+	print("\n\nOk, " + player_bird.owner + " your new " + petType + " is named " + player_bird.name + "!")
+	currentPet = player_bird
+elif typeChoice == 6:
+	player_fish = Fish(name, owner)
+	print("\n\nOk, " + player_fish.owner + " your new " + petType + " is named " + player_fish.name + "!")
+	currentPet = player_fish
+elif typeChoice == 7:
+    player_unicorn = Unicorn(name, owner)
+    print("\n\nOk, " + player_unicorn.owner + " your new " + petType + " is named" + player_unicorn.name + "!")
+    currentPet = player_unicorn
 else:
-    print("Sorry, pypets is in alpha. Only dogs and cats are supported at this time")
-    exit()
+	print("Sorry, that isn't a valid option")
+	exit()
 
 # Get and peform desired action
+
+print("\n\nWhat would you like to do with " + currentPet.name + "?")
+
 actionMenuNumber = 1
 for action in petActions:
-    print(str(actionMenuNumber) + " " + action)
-    actionMenuNumber += 1
+	print(str(actionMenuNumber) + " " + action)
+	actionMenuNumber += 1
 
 actionChoice = int(input("number: "))
 actionType = petActions[actionChoice-1]
