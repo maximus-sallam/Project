25aa752e0b1d72590dc9fc1c0fd1f672b3f8ea09
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 11:56:58 +0800

    completed 4-13

---
+++ buffet.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3.7
+foods = ('burger','taco','pizza','hotdog','pokpok')
+
+for food in foods:
+    print(food)
+
+#foods[0] = 'turd'
+
+foods = ('blowjobs','tits','ass','pussy','pokpok')
+
+print("\n")
+for food in foods:
+    print(food)
