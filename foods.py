415e164a5ae026e3e362a0607f47cabb1ab22787
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-20 11:10:13 +0800

    completed 4-12

--- foods.py
+++ foods.py
@@ -3,7 +3,7 @@
 my_foods = ['pizza','burger','cake','pie','tacos']
 friend_foods = my_foods[:]
 
-my_foods.append('pancakes')
+my_foods.append('pokpok')
 friend_foods.append('turds')
 
 print("My favorite foods are:")
@@ -11,7 +11,6 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are:")
 print(friend_foods)
-
 
 print("\nThe first three items in the list are:")
 print(my_foods[:3])
@@ -21,3 +20,11 @@ print(my_foods[1:4])
 
 print("\nThe last three items in the list are:")
 print(my_foods[-3:])
+
+for food in my_foods:
+    print("\nHave I ever had a " + food + "? Yes, I have!")
+
+for ffood in friend_foods:
+    print("\nHave you ever had a " + ffood + "?")
+
+
