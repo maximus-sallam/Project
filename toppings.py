d339aaed67abe72dfde66581fa424220f437beca
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-22 20:24:43 +0800

    modified toppings.py to do more stuff

--- toppings.py
+++ toppings.py
@@ -1,5 +1,17 @@
 #!/usr/local/bin/python3.7
-topping = 'mushrooms'
+toppings = ['mushrooms', 'onions', 'pineapple']
 
-if topping != 'anchovies':
-    print("Hold the anchovies!")
+if 'mushrooms' in toppings:
+    print('Yay!')
+
+if 'pepperoni' in toppings:
+    print('Great!')
+
+else:
+    print('Oh no!')
+
+if toppings != 'anchovies':
+    print('Hold the anchovies!')
+
+else:
+    print('WTF!')
