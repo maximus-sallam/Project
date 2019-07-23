85713805d60edb0c5defcbeb5429505dfb052859
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-23 20:29:39 +0800

    added amusement_park.py from page 85

---
+++ amusement_park.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3.7
+age = 12
+
+if (age < 4):
+    print("Your admission cost is $0.")
+
+elif (age < 18):
+    print("Your adminssion cost is $5.")
+
+else:
+    print("Your adminssion cost is $10.")
