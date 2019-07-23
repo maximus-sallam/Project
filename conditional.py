25ca4d1b0e38905abb7e7aa27d375ca8d9927302
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-23 19:52:22 +0800

    completed example 5-1

--- conditional.py
+++ conditional.py
@@ -5,3 +5,33 @@ print(car == 'suburu')
 
 print("\nIs car == 'audi'? I predict False.")
 print(car == 'audi')
+
+problem_one = (3 + 6) * 2
+problem_two = (3 ** 2) * 2
+
+print('\n')
+print((3 + 6) * 2 >= (3 ** 2) * 2)
+print((3 + 6) * 2 <= (3 ** 2) * 2)
+print((3 + 6) * 2 == (3 ** 2) * 2)
+
+print(problem_one >= problem_two)
+print(problem_one <= problem_two)
+print(problem_one == problem_two)
+
+print(problem_one >= (3 + 6) * 2)
+print(problem_one <= (3 + 6) * 2)
+print(problem_one == (3 + 6) * 2)
+
+print(problem_one >= ((3 ** 2) * 2))
+print(problem_one <= ((3 ** 2) * 2))
+print(problem_one == ((3 ** 2) * 2))
+
+print(problem_two >= (3 + 6) * 2)
+print(problem_two <= (3 + 6) * 2)
+print(problem_two == (3 + 6) * 2)
+
+print(problem_two >= ((3 ** 2) * 2))
+print(problem_two <= ((3 ** 2) * 2))
+print(problem_two == ((3 ** 2) * 2))
+
+
