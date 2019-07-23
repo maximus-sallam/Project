b10223593c32eceef2b258ee2880216dbc45e845
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-23 19:39:21 +0800

    added example 5-1 called conditional.py

---
+++ conditional.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.7
+car = 'suburu'
+print("Is car == 'suburu'? I predict True.")
+print(car == 'suburu')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
