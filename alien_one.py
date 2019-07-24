6b373e26136a58b5016b71939fb483ce0ba48940
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-24 13:21:59 +0800

    added alien_one.py and alien_two.py from examples 5-3 and 5-4

---
+++ alien_one.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3.7
+
+alien_color = 'green'
+
+if (alien_color == 'green'):
+    print("Player earned 5 points")
+
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("Player earned 5 points")
+
+
