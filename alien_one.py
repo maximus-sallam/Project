6fa6b70a3e573b449f3203563f446655216429ec
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-24 13:28:54 +0800

    merged alien_two.py and alien_one.py then deleted alien_two.py

--- alien_one.py
+++ alien_one.py
@@ -11,3 +11,19 @@ if (alien_color == 'green'):
     print("Player earned 5 points")
 
 
+#!/usr/local/bin/python3.7
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("Player has earned 5 points!")
+else:
+    print("Player has earned 10 points!")
+
+
+if (alien_color == 'red'):
+    point = 5
+else:
+    point = 10
+
+print("Player has earned " + str(point) + " points!")
+
