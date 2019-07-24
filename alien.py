107d03842a6849259545f44d6966d0801d3acdf2
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-24 13:30:56 +0800

    not sure if renaming it worked

---
+++ alien.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3.7
+
+alien_color = 'green'
+
+if (alien_color == 'green'):
+    print("Player earned 5 points")
+
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("Player earned 5 points")
+
+
+#!/usr/local/bin/python3.7
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("Player has earned 5 points!")
+else:
+    print("Player has earned 10 points!")
+
+
+if (alien_color == 'red'):
+    point = 5
+else:
+    point = 10
+
+print("Player has earned " + str(point) + " points!")
+
