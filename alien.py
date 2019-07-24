72afac12ea1195722e3c02335a51fa298b5cfb9f
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-24 19:30:24 +0800

    completed example 5-5 on page 89

--- alien.py
+++ alien.py
@@ -3,16 +3,14 @@
 alien_color = 'green'
 
 if (alien_color == 'green'):
-    print("Player earned 5 points")
+    print("Player has earned 5 points!")
+
 
 alien_color = 'red'
 
 if (alien_color == 'green'):
-    print("Player earned 5 points")
+    print("Player has earned 5 points!")
 
-
-#!/usr/local/bin/python3.7
-alien_color = 'red'
 
 if (alien_color == 'green'):
     print("Player has earned 5 points!")
@@ -20,10 +18,53 @@ else:
     print("Player has earned 10 points!")
 
 
-if (alien_color == 'red'):
-    point = 5
-else:
-    point = 10
+alien_color = 'green'
 
+if (alien_color == 'green'):
+    point = 5
+elif (alien_color == 'yellow'):
+    point = 10
+elif (alien_color == 'red'):
+    point = 15
+else:
+    point = 0
 print("Player has earned " + str(point) + " points!")
 
+
+alien_color = 'yellow'
+
+if (alien_color == 'green'):
+    point = 5
+elif (alien_color == 'yellow'):
+    point = 10
+elif (alien_color == 'red'):
+    point = 15
+else:
+    point = 0
+print("Player has earned " + str(point) + " points!")
+
+
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    point = 5
+elif (alien_color == 'yellow'):
+    point = 10
+elif (alien_color == 'red'):
+    point = 15
+else:
+    point = 0
+print("Player has earned " + str(point) + " points!")
+
+
+alien_color = 'blue'
+
+if (alien_color == 'green'):
+    point = 5
+elif (alien_color == 'yellow'):
+    point = 10
+elif (alien_color == 'red'):
+    point = 15
+else:
+    point = 0
+print("Player has earned " + str(point) + " points!")
