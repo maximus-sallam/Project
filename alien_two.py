3d2b4a424f4826bbefb3e7e0bad63e8c7e519380
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-24 13:27:32 +0800

    completed examples 5-3 and 5-4 on page 88

--- alien_two.py
+++ alien_two.py
@@ -1,1 +1,16 @@
 #!/usr/local/bin/python3.7
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("Player has earned 5 points!")
+else:
+    print("Player has earned 10 points!")
+
+
+if (alien_color == 'red'):
+    point = 5
+else:
+    point = 10
+
+print("Player has earned " + str(point) + " points!")
+
