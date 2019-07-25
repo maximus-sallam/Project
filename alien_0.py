8724456d8b2879b38de8977fd21a4b1fb5151673
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-26 07:15:32 +0800

    modified alien_0.py to increment x position per page 99

--- alien_0.py
+++ alien_0.py
@@ -8,9 +8,30 @@ alien_0['points'] = '5'
 
 print(alien_0['color'])
 print(alien_0['points'])
-print(alien_0)
+#print(alien_0)
 
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
+#print(alien_0)
 
-print(alien_0)
+print("The alien is " + alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+
+alien_0['speed'] = 'medium'
+print("Original x-position: " + str(alien_0['x_position']))
+
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New x-position: " + str(alien_0['x_position']))
