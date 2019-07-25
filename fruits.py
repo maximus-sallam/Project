a356ad052ffbd48fe382612b1b0318839a4da36e
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-25 08:50:01 +0800

    completed example 5-7 on page 89

--- fruits.py
+++ fruits.py
@@ -1,1 +1,32 @@
 #!/usr/local/bin/python3.7
+
+favorite_fruits = ['strawberry', 'pineapple', 'mango', 'watermelon']
+
+shis = "Shove this "
+inyom = " in your mouth and suck it!"
+idly = "I don't like you!"
+
+if 'strawberry' in favorite_fruits:
+    fruit = 'strawberry'
+    print(shis + fruit + inyom)
+else:
+    print(idly)
+
+if 'pineapple' in favorite_fruits:
+    fruit = 'pineapple'
+    print(shis + fruit + inyom)
+else:
+    print(idly)
+
+if 'mango' in favorite_fruits:
+    fruit = 'mango'
+    print(shis + fruit + inyom)
+else:
+    print(idly)
+
+if 'watermelon' in favorite_fruits:
+    fruit = 'mango'
+    print(shis + fruit + inyom)
+else:
+    print(idly)
+
