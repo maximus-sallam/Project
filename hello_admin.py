9657b6d3307eb9306bfb75015f4f12534c8c9341
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-25 09:45:31 +0800

    completed example 5-9 on page 93

--- hello_admin.py
+++ hello_admin.py
@@ -1,18 +1,22 @@
 #!/usr/local/bin/python3.7
 
 valid_users = ['admin', 'zebra', 'max', 'joe', 'alex']
-entered_users = ['AdmIN', 'ZEBRA', 'mr. potato head', 'MAx', 'captain thunderpants', 'Joe', 'Alex']
+#entered_users = ['AdmIN', 'ZEBRA', 'mr. potato head', 'MAx', 'captain thunderpants', 'Joe', 'Alex']
+entered_users = []
 
 welcome = 'Welcome back '
 admin = '! Would you like to see a status report?'
 other = '! It\'s great to see you again.'
 who = 'Who the hell are you, '
+no = 'No users were entered.'
 
-for entered_user in entered_users:
-    if entered_user.lower() in valid_users and entered_user.lower() == 'admin':
-        print(welcome + entered_user.title() + admin)
-    elif entered_user.lower() in valid_users:
-        print(welcome + entered_user.title() + other)
-    else:
-        print(who + entered_user.title() + "?!")
-
+if entered_users:
+    for entered_user in entered_users:
+        if entered_user.lower() in valid_users and entered_user.lower() == 'admin':
+            print(welcome + entered_user.title() + admin)
+        elif entered_user.lower() in valid_users:
+            print(welcome + entered_user.title() + other)
+        else:
+            print(who + entered_user.title() + "?!")
+else:
+    print(no)
