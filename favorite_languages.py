5e1cc12167a526e351c4743bfe2555cec727ef0b
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-26 07:37:39 +0800

    added favorite_languages.py from page 101

---
+++ favorite_languages.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3.7
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+print("Sarah's favorite language is " +
+    favorite_languages['sarah'].title() +
+    ".")
+
