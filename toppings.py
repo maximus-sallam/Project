c5569fb156f7f39ef5544eb0f1d07ec53238464c
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-25 09:22:43 +0800

    modified toppings.py as per page 92

--- toppings.py
+++ toppings.py
@@ -1,11 +1,19 @@
 #!/usr/local/bin/python3.7
-requested_toppings = ['mushrooms', 'onions', 'pineapple', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+
+
+#if 'mushrooms' in requested_toppings:
+#    print("Adding mushrooms.")
+#if 'pepperoni' in requested_toppings:
+#    print("Adding pepperoni")
+#if 'extra cheese' in requested_toppings:
+#    print("Adding extra cheese.")
 
 print("\nFinished making your pizza!")
