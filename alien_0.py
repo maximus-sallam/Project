6e333531fd5c77bb78f101dfc391221962581c60
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-25 19:13:42 +0800

    updated alien_0.py as per page 98

--- alien_0.py
+++ alien_0.py
@@ -1,6 +1,10 @@
 #!/usr/local/bin/python3.7
 
-alien_0 = {'color': 'green', 'points': 5}
+#alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = '5'
 
 print(alien_0['color'])
 print(alien_0['points'])
@@ -10,4 +14,3 @@ alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 
 print(alien_0)
-
