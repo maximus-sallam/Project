506175d2451fb5eb173416e05be275a663d84d39
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-27 07:54:01 +0800

    added alien1.py and modified per page 109

---
+++ alien1.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.7
+
+alien_0 = {'color': 'green'}
+alien_1 = {'color': 'yellow'}
+alien_2 = {'color': 'red'}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
