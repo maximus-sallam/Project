8ee9258a88bc788b18f3077fb5f71233881e0bca
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-26 08:13:38 +0800

    changed age to birthday in example 6-1 on page 102

--- person.py
+++ person.py
@@ -3,7 +3,7 @@
 person = {
     'first_name': 'joe',
     'last_name': 'higgins',
-    'age': '29',
+    'birthday': 'may 9',
     'city': 'stockholm',
     }
 
@@ -12,8 +12,8 @@ for key in person:
         print("Meet " + person[key].title())
     if key == 'last_name':
         print("He's a member of the " + person[key].title() + " clan.")
-    if key == 'age':
-        print("He's " + person[key] + " years old")
+    if key == 'birthday':
+        print("His birthday is on " + person[key] + "th.")
     if key == 'city':
         print("He lives in " + person[key].title())
 
