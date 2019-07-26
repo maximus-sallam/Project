9587144882e82ab9b8dfed5b4c3309dedda293de
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-26 09:40:33 +0800

    added user.py form page 103

---
+++ user.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3.7
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
