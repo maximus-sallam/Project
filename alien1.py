390fd480b295bf083aabb4b4374c8cb0b5510f5a
Maximus Sallam <maximus.sallam@gmail.com>
2019-07-27 08:01:43 +0800

    modified alien1.py per page 109-110

--- alien1.py
+++ alien1.py
@@ -8,3 +8,22 @@ aliens = [alien_0, alien_1, alien_2]
 
 for alien in aliens:
     print(alien)
+
+# Make an empty list for storing alients.
+aliens = []
+
+# Make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color': 'green',
+                'points': 5,
+                'speed': 'slow',
+                }
+    aliens.append(new_alien)
+
+# Show the first 5 aliiens.
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
