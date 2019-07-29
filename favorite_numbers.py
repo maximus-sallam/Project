5a1bd2911e559a61e962ae49a9ac07e45d2d9401
maximus-sallam <maximus.sallam@gmail.com>
2019-07-29 01:29:02 -0400

    completed example 6-10 on page 115

--- favorite_numbers.py
+++ favorite_numbers.py
@@ -1,24 +1,24 @@
 #!/usr/local/bin/python3.7
 
 numbers = {
-    'joe': '9',
-    'alex': '6',
-    'max': '69',
-    'ven': '7',
-    'cortney': '8',
+    'joe': ['9', '18', '27'],
+    'alex': ['6','66', '666'],
+    'max': ['69', '42', '64'],
+    'ven': ['7', '21', '5'],
+    'cortney': ['8', '4', '2'],
     }
 
 fav = '\'s favorite number is '
 
-for key in numbers:
+for key, value in numbers.items():
     if key == 'joe':
-        print(key.title() + fav + str(numbers[key]))
+        print(key.title() + fav, ", ".join(numbers[key]))
     if key == 'alex':
-        print(key.title() + fav + numbers[key])
+        print(key.title() + fav, ", ".join(numbers[key]))
     if key == 'max':
-        print(key.title() + fav + numbers[key] + ". lol")
+        print(key.title() + fav, ", ".join(numbers[key]) + ". lol")
     if key == 'ven':
-        print(key.title() + fav + numbers[key])
+        print(key.title() + fav, ", ".join(numbers[key]))
     if key == 'cortney':
-        print(key.title() + fav + numbers[key])
+        print(key.title() + fav, ", ".join(numbers[key]))
 
