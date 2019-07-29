e68d85a3a69e3d27b8813e764fed455a7e24b636
maximus-sallam <maximus.sallam@gmail.com>
2019-07-28 22:07:41 -0400

    leaned up the output by removing an unnecessary space

--- person.py
+++ person.py
@@ -1,22 +1,22 @@
 #!/usr/local/bin/python3.7
 
 jHiggins = {
-    'first_name': 'joe',
-    'last_name': 'higgins',
+    'firstName': 'joe',
+    'lastName': 'higgins',
     'birthday': 'may 9',
     'city': 'stockholm',
 }
 
 aMamalakis = {
-    'first_name': 'alex',
-    'last_name': 'mamalakis',
+    'firstName': 'alex',
+    'lastName': 'mamalakis',
     'birthday': 'may 16',
     'city': 'houston',
 }
 
 cRawls = {
-    'first_name': 'camron',
-    'last_name': 'rawls',
+    'firstName': 'camron',
+    'lastName': 'rawls',
     'birthday': 'march 17',
     'city': 'hick town',
 }
@@ -24,8 +24,8 @@ cRawls = {
 people = [jHiggins, aMamalakis, cRawls]
 
 for userInfo in people:
-    print(userInfo['first_name'].title(),
-          userInfo['last_name'].title(), "is from",
+    print(userInfo['firstName'].title(),
+          userInfo['lastName'].title(), "is from",
           userInfo['city'].title() +
           ". His birthday is",
           userInfo['birthday'].title())
