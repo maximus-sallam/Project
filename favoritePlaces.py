8f255c8b21362e752927c24cfe9ed401e517cdb2
maximus-sallam <maximus.sallam@gmail.com>
2019-07-29 00:54:10 -0400

    completed example 6-9 on page 115

---
+++ favoritePlaces.py
@@ -0,0 +1,9 @@
+favorite_places = {
+    'joe': ['new york', 'michigan', 'sweden'],
+    'alex': ['texas', 'hell', 'michigan'],
+    'camron': ['louisiana', 'philippines', 'california'],
+    'cortney': ['sweden', 'michigan', 'california'],
+}
+
+for name in favorite_places:
+    print(name + "'s favorite places are", ", ".join(favorite_places[name]))
