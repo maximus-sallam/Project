e7df0e7310cbdba0eaa05f19f7b21bffeb0d5cab
maximus-sallam <maximus.sallam@gmail.com>
2019-07-28 23:32:47 -0400

    completed example 6-8 on page 115

---
+++ pets.py
@@ -0,0 +1,22 @@
+riley = {
+    'animalKind': 'dog',
+    'ownerName': 'ven',
+    'age': '3',
+}
+
+bucky = {
+    'animalKind': 'demon',
+    'ownerName': 'venice',
+    'age': '1',
+}
+
+alden = {
+    'animalKind': 'prisoner',
+    'ownerName': 'chito',
+    'age': '4',
+}
+
+pets = [alden, bucky, riley]
+
+for pet in pets:
+    print(pet)
