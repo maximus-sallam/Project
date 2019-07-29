95a780eb2e52f608678561d70ebb75c0cd9362e5
maximus-sallam <maximus.sallam@gmail.com>
2019-07-28 22:10:15 -0400

    cleaned up the formatting

--- person.py
+++ person.py
@@ -1,26 +1,22 @@
 #!/usr/local/bin/python3.7
-
 jHiggins = {
     'firstName': 'joe',
     'lastName': 'higgins',
     'birthday': 'may 9',
     'city': 'stockholm',
 }
-
 aMamalakis = {
     'firstName': 'alex',
     'lastName': 'mamalakis',
     'birthday': 'may 16',
     'city': 'houston',
 }
-
 cRawls = {
     'firstName': 'camron',
     'lastName': 'rawls',
     'birthday': 'march 17',
     'city': 'hick town',
 }
-
 people = [jHiggins, aMamalakis, cRawls]
 
 for userInfo in people:
