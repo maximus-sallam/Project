c914061c5e2bda77365acae2c1342f49e228b6e9
maximus-sallam <maximus.sallam@gmail.com>
2019-07-29 20:48:39 -0400

    added parrot.py. combined contents of parrot.py and greeter.py from pages 118-119

---
+++ parrot.py
@@ -0,0 +1,18 @@
+#message = input("Tell me something, and I will repeat it back to you: ")
+#print(message)
+
+#name = input("Please tell me your name: ")
+#print("Hello, " + name.title() + "! Allow me to suck your penis")
+
+#prompt = "If you tell us who you are, we can personalize the messages you see."
+#prompt += "\nWhat is your first name? "
+
+#name = input(prompt)
+#print("\nHello, " + name.title() + "!")
+
+age = int(input("How old are you? "))
+#age = int(age)
+if age >= 18:
+    print(age)
+else:
+    print("minor")
