14c7e8374574448437cb55515c32d5a3dca9dcab
maximus-sallam <maximus.sallam@gmail.com>
2019-07-30 18:31:58 -0400

    modified counting.py per page 126-127

--- counting.py
+++ counting.py
@@ -1,16 +1,23 @@
-#current_number = -5
-#while current_number <= 5:
-#    print(current_number)
-#    current_number += 1
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
 
-prompt = "\nTell me something, and I will repeat it back to you: "
-prompt += "\nEnter 'quit' to end the program. "
+    print(current_number)
 
-active = True
-while active:
-    message = input(prompt)
-    if message == 'quit':
-        active = False
-    else:
-        print(message)
+#prompt = "\nTell me something, and I will repeat it back to you: "
+#prompt += "\nEnter 'quit' to end the program. "
 
+#active = True
+#while active:
+#    message = input(prompt)
+#    if message == 'quit':
+#        active = False
+#    else:
+#        print(message)
+
+x= 1
+while x <= 5:
+       print(x)
+       x += 1
