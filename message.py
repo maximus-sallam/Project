e29b707f221d1dff7ad6d7617c1f90c1112f8c50
maximus-sallam <maximus.sallam@gmail.com>
2019-07-31 19:07:26 -0400

    completed example 8-1 and 8-2 on page 135

---
+++ message.py
@@ -0,0 +1,9 @@
+def display_message():
+    print("I'm learning about functions in chapter 8")
+
+display_message()
+
+def favorite_book(title):
+    print("One of my favorite books is " + title.title())
+
+favorite_book('Python: A Crash Course')
