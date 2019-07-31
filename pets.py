b3d8a94d63fdc385b488941ce46fc3a6064c444a
maximus-sallam <maximus.sallam@gmail.com>
2019-07-31 19:30:11 -0400

    added function to pets.py per page 136

--- pets.py
+++ pets.py
@@ -1,31 +1,39 @@
-riley = {
-    'animalKind': 'dog',
-    'ownerName': 'ven',
-    'age': '3',
-}
+# riley = {
+#     'animalKind': 'dog',
+#     'ownerName': 'ven',
+#     'age': '3',
+# }
 
-bucky = {
-    'animalKind': 'demon',
-    'ownerName': 'venice',
-    'age': '1',
-}
+# bucky = {
+#     'animalKind': 'demon',
+#     'ownerName': 'venice',
+#     'age': '1',
+# }
 
-alden = {
-    'animalKind': 'prisoner',
-    'ownerName': 'chito',
-    'age': '4',
-}
+# alden = {
+#     'animalKind': 'prisoner',
+#     'ownerName': 'chito',
+#     'age': '4',
+# }
 
-pets = [alden, bucky, riley]
+# pets = [alden, bucky, riley]
 
-for pet in pets:
-    for key, value in pet.items():
-        print(key.title(), value.title())
+# for pet in pets:
+#     for key, value in pet.items():
+#         print(key.title(), value.title())
 
-pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
-print(pets)
+# pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+# print(pets)
 
-while 'cat' in pets:
-    pets.remove('cat')
+# while 'cat' in pets:
+#     pets.remove('cat')
 
-print(pets)
+# print(pets)
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'riley')
