1450ede3599c2f7b883d03d8dae95890972cfaf5
maximus-sallam <maximus.sallam@gmail.com>
2019-07-31 19:54:24 -0400

    updated functions in pets.py per page 136-138

--- pets.py
+++ pets.py
@@ -30,10 +30,13 @@
 
 # print(pets)
 
-def describe_pet(animal_type, pet_name):
+def describe_pet(animal_type='pussy', pet_name='delicious'):
     """Display information about a pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
 describe_pet('hamster', 'harry')
 describe_pet('dog', 'riley')
+describe_pet(pet_name='dick', animal_type='penis')
+describe_pet()
+describe_pet(pet_name='yummy')
