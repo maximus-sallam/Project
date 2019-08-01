8e70161ad8af72a81c4b19610aebaa27f08ab5df
maximus-sallam <maximus.sallam@gmail.com>
2019-07-31 20:25:46 -0400

    completed examples 8-3 and 8-4 on page 141

---
+++ tshirt.py
@@ -0,0 +1,10 @@
+def make_shirt(size='large', text='I love Python'):
+    print("\nYou selected size " + size + ".")
+    print("'" + text + "' will be printed on the shirt.")
+
+make_shirt('small', 'I have to poop')
+make_shirt(size='x-small', text='Eat my pussy')
+make_shirt()
+make_shirt(size='medium')
+make_shirt(size='medium', text="Gotta catch'em all")
+
