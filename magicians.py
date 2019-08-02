1671e312e173cf165936d9bc8f4eea3b517d656d
maximus-sallam <maximus.sallam@gmail.com>
2019-08-01 21:40:25 -0400

    completed example 8-9 on page 150

---
+++ magicians.py
@@ -0,0 +1,7 @@
+def show_magicians(magician_list):
+    for magician in magician_list:
+        print(magician)
+
+magicians = ['merlin', 'houdini', 'magical max', 'gandolf the grey', 'ash ketchum']
+
+show_magicians(magicians)
