d7d5b2079a4f3e6b2f4d479d870700ae4f4af087
maximus-sallam <maximus.sallam@gmail.com>
2019-08-02 04:16:44 -0400

    went back and updated example 8-9 and 8-10 on page 150 again

--- magicians.py
+++ magicians.py
@@ -1,11 +1,16 @@
-def show_magicians(magician_list):
-    while magician_list:
-        current_magician = magician_list.pop()
-        print(current_magician)
-
-
-
 magicians = ['merlin', 'houdini', 'magical max', 'gandolf the grey', 'ash ketchum']
-great_magicians = []
 
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician.title())
+
+def make_great(magicians):
+    count = 0
+    for magician in magicians:
+        magicians[count] = 'the great ' + magician
+        count += 1
+    return magicians
+
+show_magicians(magicians)
+make_great(magicians)
 show_magicians(magicians)
