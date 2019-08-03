54ddf1309a77ed9345ede8093771f9769fdd4902
maximus-sallam <maximus.sallam@gmail.com>
2019-08-03 00:48:15 -0400

    completed example 9-2 on page 166

--- restauraunt.py
+++ restauraunt.py
@@ -7,14 +7,27 @@ class Restaurant():
 
     def describe_retaurant(self):
         print("\nWelcome to " + self.restaurant_name.title() + "!")
-        print("Our specialty is " + self.cuisine_type + " food.")
+        print("Our specialty is " + self.cuisine_type)
 
     def open_restaurant(self):
         print(self.restaurant_name.title() + " is now open for business.")
 
-restaurant = Restaurant('pokpok palace', 'puki')
+pokpokpalace = Restaurant('pokpok palace', 'pleasing you')
+vaginaisland = Restaurant('vagina island', 'puki')
+hookerfarms = Restaurant('hooker farms', 'riding your dick')
+girlsgalore = Restaurant('girls galore', 'making your penis happy')
 
-print(restaurant.restaurant_name)
-print(restaurant.cuisine_type)
-restaurant.describe_retaurant()
-restaurant.open_restaurant()
+print(pokpokpalace.restaurant_name)
+print(pokpokpalace.cuisine_type)
+
+pokpokpalace.describe_retaurant()
+pokpokpalace.open_restaurant()
+
+vaginaisland.describe_retaurant()
+vaginaisland.open_restaurant()
+
+hookerfarms.describe_retaurant()
+hookerfarms.open_restaurant()
+
+girlsgalore.describe_retaurant()
+girlsgalore.open_restaurant()
