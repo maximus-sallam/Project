c5e7b2d704f8f8a78403022f8bdfb2fe7ee643dc
maximus-sallam <maximus.sallam@gmail.com>
2019-08-02 20:15:13 -0400

    completed example 8-14 on page 154

--- cars.py
+++ cars.py
@@ -1,12 +1,23 @@
 #!/usr/local/bin/python3.7
 
-cars = ['audi', 'bmw', 'suburu', 'toyota']
+# cars = ['audi', 'bmw', 'suburu', 'toyota']
+#
+# for car in cars:
+#     if (car == 'bmw'):
+#         print(car.upper())
+#     else:
+#         print(car.title())
+#
+#     if (car == 'suburu'):
+#         print('lol')
 
-for car in cars:
-    if (car == 'bmw'):
-        print(car.upper())
-    else:
-        print(car.title())
+def build_car(manufacturer, model, **car_info):
+    car = {}
+    car['manufacturer'] = manufacturer
+    car['model'] = model
+    for key, value in car_info.items():
+        car[key] = value
+    return car
 
-    if (car == 'suburu'):
-        print('lol')
+car = build_car('suburu', 'outback', color='blue', tow_package=True)
+print(car)
