639093deccd62950de1f6ee1e1291f0b4bab3112
maximus-sallam <maximus.sallam@gmail.com>
2019-08-02 21:06:13 -0400

    updated making_pizza.py per page 156

--- making_pizza.py
+++ making_pizza.py
@@ -1,4 +1,5 @@
-import pizza
+from pizza import make_pizza as mp
 
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
