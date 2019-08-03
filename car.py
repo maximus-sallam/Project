d906fd41ddf825fd4635221a87de3f04050594cd
maximus-sallam <maximus.sallam@gmail.com>
2019-08-03 18:55:16 -0400

    cleaned up formatting

--- car.py
+++ car.py
@@ -85,10 +85,12 @@ class ElectricCar(Car):
 
 
 # my_tesla = ElectricCar('tesla', 'model s', 2016)
+
 # print(my_tesla.get_descriptive_name())
 # my_tesla.describe_battery()
 
 my_used_car = Car('subaru', 'outback', 2013)
+
 print(my_used_car.get_descriptive_name())
 my_used_car.read_gas_tank()
 my_used_car.read_odometer()
@@ -104,11 +106,12 @@ my_used_car.drive_somewhere(25)
 # my_used_car.increment_odometer(100)
 # my_used_car.read_odometer()
 
-# """Attempting to roll the odometer back."""
+"""Attempting to roll the odometer back."""
 # my_used_car.update_odometer(23500)
 # my_used_car.read_odometer()
 
 # my_new_car = Car('audi', 'a4', 2016)
+
 # print(my_new_car.get_descriptive_name())
 
 # my_new_car.odometer_reading = 23
