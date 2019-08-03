182e09ec23a128542171a76fa2c8231ce047ea8c
maximus-sallam <maximus.sallam@gmail.com>
2019-08-03 06:32:40 -0400

    modified car.py per page 169-171

--- car.py
+++ car.py
@@ -17,6 +17,35 @@ class Car():
         """Print a statement showing the car's mileage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def incement_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+my_used_car.incement_odometer(100)
+my_used_car.read_odometer()
+
+"""Attempting to roll the odometer back."""
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.odometer_reading = 23
+# my_new_car.update_odometer(22)
+# my_new_car.read_odometer()
