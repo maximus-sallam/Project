e026a09d8b69b02542f073f35f017ae9e37be72d
maximus-sallam <maximus.sallam@gmail.com>
2019-08-03 20:03:59 -0400

    updated the battery class per page 177 and cleaned up some formatting.

--- car.py
+++ car.py
@@ -79,6 +79,16 @@ class Battery():
         """Print a statement describing the battery size."""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
@@ -95,16 +105,20 @@ class ElectricCar(Car):
         """Electric cars don't have gas tanks."""
         print("This car doesn't need gas!")
 
+
 my_tesla = ElectricCar('tesla', 'model s', 2016)
+my_new_car = Car('audi', 'a4', 2016)
+my_used_car = Car('subaru', 'outback', 2013)
+
 
 print(my_tesla.get_descriptive_name())
 
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 my_tesla.fill_gas_tank()
+my_tesla.drive_somewhere(25)
 
 
-# my_used_car = Car('subaru', 'outback', 2013)
-#
 # print(my_used_car.get_descriptive_name())
 
 # my_used_car.read_gas_tank()
@@ -126,7 +140,6 @@ my_tesla.fill_gas_tank()
 # my_used_car.update_odometer(23500)
 # my_used_car.read_odometer()
 
-# my_new_car = Car('audi', 'a4', 2016)
 
 # print(my_new_car.get_descriptive_name())
 
