7c017ffab70da25e74d5c2967ceb8d5ea62eed13
maximus-sallam <maximus.sallam@gmail.com>
2019-08-05 19:47:27 -0400

    commented out calls to methods from car.py per page 179 and modified my_car.py per page 180

--- car.py
+++ car.py
@@ -115,20 +115,20 @@ class ElectricCar(Car):
         print("This car doesn't need gas!")
 
 
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-my_new_car = Car('audi', 'a4', 2016)
-my_used_car = Car('subaru', 'outback', 2013)
-
-
-print(my_tesla.get_descriptive_name())
-
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.fill_gas_tank()
-my_tesla.drive_somewhere(25)
+# my_tesla = ElectricCar('tesla', 'model s', 2016)
+# my_new_car = Car('audi', 'a4', 2016)
+# my_used_car = Car('subaru', 'outback', 2013)
+#
+#
+# print(my_tesla.get_descriptive_name())
+#
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
+# my_tesla.battery.upgrade_battery()
+# my_tesla.battery.get_range()
+# my_tesla.battery.upgrade_battery()
+# my_tesla.fill_gas_tank()
+# my_tesla.drive_somewhere(25)
 
 
 # print(my_used_car.get_descriptive_name())
@@ -148,7 +148,7 @@ my_tesla.drive_somewhere(25)
 # my_used_car.increment_odometer(100)
 # my_used_car.read_odometer()
 
-"""Attempting to roll the odometer back."""
+# """Attempting to roll the odometer back."""
 # my_used_car.update_odometer(23500)
 # my_used_car.read_odometer()
 
