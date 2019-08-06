cc4be878dc4eceedaa8a042e1875bfc9e66de1f6
maximus-sallam <maximus.sallam@gmail.com>
2019-08-05 21:05:49 -0400

    modified my_car.py per pages 181-182

--- my_car.py
+++ my_car.py
@@ -1,9 +1,19 @@
-from car import Car
+# from car import Car, ElectricCar
+import car
 
-my_new_car = Car('audi', 'a4', 2016)
+my_beetle = car.Car('volkswagon', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
 
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-my_new_car.fill_gas_tank()
-my_new_car.drive_somewhere(25)
+my_tesla = car.ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
+
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+# my_new_car.fill_gas_tank()
+# my_new_car.drive_somewhere(25)
+
