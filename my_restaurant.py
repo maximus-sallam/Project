b3a2ce374cd7ba83601ecbb459d38d7f823c7241
maximus-sallam <maximus.sallam@gmail.com>
2019-08-06 07:26:02 -0400

    moved all call to methods from restaurant.py to my_restaurant.py

--- my_restaurant.py
+++ my_restaurant.py
@@ -4,3 +4,37 @@ my_resto = restaurant.IceCreamStand('massagy boom boom', 'rubbing your dick')
 my_resto.describe_retaurant()
 my_resto.open_restaurant()
 my_resto.display_flavors()
+
+pokpokpalace = restaurant.Restaurant('pokpok palace', 'pleasing you')
+vaginaisland = restaurant.Restaurant('vagina island', 'puki')
+hookerfarms = restaurant.Restaurant('hooker farms', 'riding your dick')
+girlsgalore = restaurant.Restaurant('girls galore', 'making your penis happy')
+
+print(my_resto.number_served)
+my_resto.number_served = 23
+my_resto.set_number_served(21)
+my_resto.set_number_served(25)
+print(my_resto.number_served)
+my_resto.increment_number_served(25)
+print(my_resto.number_served)
+
+
+pokpokpalace = restaurant.Restaurant('pokpok palace', 'pleasing you')
+vaginaisland = restaurant.Restaurant('vagina island', 'puki')
+hookerfarms = restaurant.Restaurant('hooker farms', 'riding your dick')
+girlsgalore = restaurant.Restaurant('girls galore', 'making your penis happy')
+
+print(pokpokpalace.restaurant_name)
+print(pokpokpalace.cuisine_type)
+
+pokpokpalace.describe_retaurant()
+pokpokpalace.open_restaurant()
+
+vaginaisland.describe_retaurant()
+vaginaisland.open_restaurant()
+
+hookerfarms.describe_retaurant()
+hookerfarms.open_restaurant()
+
+girlsgalore.describe_retaurant()
+girlsgalore.open_restaurant()
--- restaurant.py
+++ restaurant.py
@@ -32,37 +32,3 @@ class IceCreamStand(Restaurant):
         print("Our available flavors are:")
         for flavor in self.flavors:
             print("- " + flavor.title())
-
-# restaurant = IceCreamStand('massagy boom boom', 'rubbing your dick')
-# restaurant.describe_retaurant()
-# restaurant.open_restaurant()
-# restaurant.display_flavors()
-
-# print(restaurant.number_served)
-# restaurant.number_served = 23
-# restaurant.set_number_served(21)
-# restaurant.set_number_served(25)
-# print(restaurant.number_served)
-# restaurant.increment_number_served(25)
-# print(restaurant.number_served)
-#
-#
-# pokpokpalace = Restaurant('pokpok palace', 'pleasing you')
-# vaginaisland = Restaurant('vagina island', 'puki')
-# hookerfarms = Restaurant('hooker farms', 'riding your dick')
-# girlsgalore = Restaurant('girls galore', 'making your penis happy')
-
-# print(pokpokpalace.restaurant_name)
-# print(pokpokpalace.cuisine_type)
-#
-# pokpokpalace.describe_retaurant()
-# pokpokpalace.open_restaurant()
-#
-# vaginaisland.describe_retaurant()
-# vaginaisland.open_restaurant()
-#
-# hookerfarms.describe_retaurant()
-# hookerfarms.open_restaurant()
-#
-# girlsgalore.describe_retaurant()
-# girlsgalore.open_restaurant()
