792909395ecc629ae8fdddb8bc26b0dff9a13d91
maximus-sallam <maximus.sallam@gmail.com>
2019-08-06 20:25:18 -0400

    updated favorite_languages2.py to use OrderedDict from collections per page 185

--- favorite_languages2.py
+++ favorite_languages2.py
@@ -1,19 +1,31 @@
 #!/usr/local/bin/python3.7
 
-favorite_languages = {
-    'jen': ['python', 'ruby'],
-    'sarah': ['c'],
-    'edward': ['ruby', 'go'],
-    'phil': ['python', 'haskell'],
-}
+# favorite_languages = {
+#     'jen': ['python', 'ruby'],
+#     'sarah': ['c'],
+#     'edward': ['ruby', 'go'],
+#     'phil': ['python', 'haskell'],
+# }
+#
+# for name, languages in favorite_languages.items():
+#     if len(languages) > 1:
+#         print("\n" + name.title() + "'s favorite languages are:")
+#         for language in languages:
+#             print("\t" + language.title())
+#     else:
+#         print("\n" + name.title() + "'s favorite languages is:")
+#         for language in languages:
+#             print("\t" + language.title())
 
-for name, languages in favorite_languages.items():
-    if len(languages) > 1:
-        print("\n" + name.title() + "'s favorite languages are:")
-        for language in languages:
-            print("\t" + language.title())
-    else:
-        print("\n" + name.title() + "'s favorite languages is:")
-        for language in languages:
-            print("\t" + language.title())
+from collections import OrderedDict
 
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
