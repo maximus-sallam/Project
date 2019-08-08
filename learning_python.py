5f159c59cba4e99294a4fc2b6be19da4eee4386c
maximus-sallam <maximus.sallam@gmail.com>
2019-08-07 21:11:00 -0400

    completed example 10-2 on page 197

--- learning_python.py
+++ learning_python.py
@@ -1,15 +1,15 @@
 filename = 'learning_python.txt'
 
 with open(filename) as file_object1:
-    print(file_object1.read())
+    print(file_object1.read().replace('Python', 'C'))
 
 with open(filename) as file_object2:
     for line in file_object2:
-        print(line.strip())
+        print(line.strip().replace('Python', 'C'))
 
 with open(filename) as file_object3:
     lines = file_object3.readlines()
 string = '\n'
 for line in lines:
     string += line
-print(string)
+print(string.replace('Python', 'C'))
