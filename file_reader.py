ceb17ef267ee6bc10740c5e22e9392f29cbc4afc
maximus-sallam <maximus.sallam@gmail.com>
2019-08-07 20:09:56 -0400

    updated file_reader.py per pi_string.py on page 194

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,11 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string)
+print(len(pi_string))
