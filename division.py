f2c6ad590107a1417d1ab6513b82277646d325d4
maximus-sallam <maximus.sallam@gmail.com>
2019-08-10 00:10:46 -0400

    updated division.py per page 201

--- division.py
+++ division.py
@@ -1,8 +1,3 @@
-# try:
-#     print(5/0)
-# except ZeroDivisionError:
-#     print("You can't divide by zero!")
-
 print("Give me two numbers, and I'll divide them.")
 print("Enter 'q' to quit.")
 
@@ -13,5 +8,11 @@ while True:
     second_number = input("Second number: ")
     if second_number == 'q':
         break
-    answer = int(first_number) / int(second_number)
-    print(answer)
+    try:
+        answer = float(first_number) / float(second_number)
+    except ValueError:
+        print("Those were not numbers!")
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
