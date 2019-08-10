03d527e58d94692d4d7d8f912a51f1c3307d00dc
maximus-sallam <maximus.sallam@gmail.com>
2019-08-10 02:10:19 -0400

    made example 10-10 on page 208 handle KeyboardInturrupt errors

--- common_words.py
+++ common_words.py
@@ -1,8 +1,15 @@
 filename = 'moby_dick.txt'
 while True:
-    word = input("What word would you like me to look for? ")
-    with open(filename) as file:
-        contents = file.read()
-    print("The word " + word + " appears " +
-          str(contents.lower().count(word)) +
-          " times in " + filename)
+    try:
+        word = input("What word would you like me to look for? ")
+        with open(filename) as file:
+            contents = file.read()
+    except KeyboardInterrupt:
+        print("\nDo that one more time to quit!")
+    except FileNotFoundError:
+        msg = "Sorry, the file " + filename + " does not exist."
+        print(msg)
+    else:
+        print("The word " + word + " appears " +
+            str(contents.lower().count(word)) +
+            " times in " + filename)
