f1297b925a46af756bc515f5dfa5774ecadbb027
maximus-sallam <maximus.sallam@gmail.com>
2019-08-10 01:52:01 -0400

    completed example 10-8 on page 208

---
+++ catsanddogs.py
@@ -0,0 +1,16 @@
+def print_animals(filename):
+    try:
+        with open(filename) as f_obj:
+            contents = f_obj.read()
+    except FileNotFoundError:
+        msg = "\nSorry, the file " + filename + " does not exist."
+        print(msg)
+        # pass
+    else:
+        print("\nFile: " + filename + "\n" + contents)
+
+filenames = ['hamsters.txt', 'cats.txt', 'dogs.txt', 'fish.txt']
+
+
+for filename in filenames:
+    print_animals(filename)
