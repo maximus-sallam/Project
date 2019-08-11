621c91632a9f823ea0d859131a131d4570151e00
maximus-sallam <maximus.sallam@gmail.com>
2019-08-11 08:07:05 -0400

    modified name_function.py and names.py to accept middle names

--- names.py
+++ names.py
@@ -5,9 +5,12 @@ while True:
     first = input("\nPlease give me a first name: ")
     if first == 'q':
         break
+    middle = input("Please give me a middle name: ")
+    if middle == 'q':
+        break
     last = input("Please give me a last name: ")
     if last == 'q':
         break
 
-    formatted_name = get_formatted_name(first, last)
+    formatted_name = get_formatted_name(first, last, middle)
     print("\tNeatly formatted name: " + formatted_name + '.')
