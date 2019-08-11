be6dc1c9f7344117f47f576a247ceefa5ece0eda
maximus-sallam <maximus.sallam@gmail.com>
2019-08-11 03:41:05 -0400

    completed example 10-13 on page 214

--- remember_me.py
+++ remember_me.py
@@ -17,18 +17,31 @@ def get_stored_username():
 def get_new_username():
     """Prompt for a new username."""
     username = input("What is your name? ")
-    username = username.title()
     with open(filename, 'w') as f_obj:
-        json.dump(username, f_obj)
-    return username
+        json.dump(username.title(), f_obj)
+    return username.title()
+
+def remember():
+    username = get_new_username()
+    print("We'll remember you when you come back, " + username + "!")
+
+def maybe():
+    username = get_stored_username()
+    maybe = input("Are you " + username + "? Enter 'yes' or 'no' ")
+    if maybe.lower() == 'yes':
+        print("Welcome back, " + username + "!")
+    elif maybe.lower() == 'no':
+        remember()
+    else:
+        print("You did not enter a valid response.")
+        greet_user()
 
 def greet_user():
     """Greet the user by name."""
     username = get_stored_username()
     if username:
-        print("Welcome back, " + username + "!")
+        maybe()
     else:
-        username = get_new_username()
-        print("We'll remember you when you come back, " + username + "!")
+        remember()
 
 greet_user()
