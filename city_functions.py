09f62575554ecbcd6e74f5c3de3c9a6335f77087
maximus-sallam <maximus.sallam@gmail.com>
2019-08-11 21:03:47 -0400

    completed example 11-2 on page 222

--- city_functions.py
+++ city_functions.py
@@ -1,5 +1,11 @@
-def city_country(city, country):
+def city_country(city, country, population=''):
     """Generate a neatly formatted sentence about a city and country."""
-    where_is = 'The city of ' + city.title() + \
-               ' is in the country of ' + country.title() + '.'
+    if population:
+        where_is = 'The city of ' + city.title() + \
+               ' is in the country of ' + country.title() + \
+               '.\nThe population of ' + city.title() + ', ' + \
+               country.title() + ' is ' + str(population) + '.'
+    else:
+        where_is = 'The city of ' + city.title() + \
+                   ' is in the country of ' + country.title() + '.'
     return where_is
--- test_cities.py
+++ test_cities.py
@@ -6,7 +6,13 @@ class NameTestCase(unittest.TestCase):
 
     def test_city_country(self):
         """Do cities like 'Manila' work?"""
-        whereis = city_country('manila', 'philippines')
-        self.assertEqual(whereis, 'The city of Manila is in the country of Philippines.')
+        where_is = city_country('manila', 'philippines')
+        self.assertEqual(where_is, 'The city of Manila is in the country of Philippines.')
+
+    def test_city_country_population(self):
+        """Do cities like 'Manila' with a known population work?"""
+        where_is = city_country('manila', 'philippines', '12,877,253')
+        self.assertEqual(where_is, 'The city of Manila is in the country of Philippines.'
+                                   '\nThe population of Manila, Philippines is 12,877,253.')
 
 unittest.main()
