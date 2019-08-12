9fdfd730a0776283e81decf568253ccc941913ce
maximus-sallam <maximus.sallam@gmail.com>
2019-08-11 20:11:30 -0400

    updated name_function.py per pages 219-220

--- names.py
+++ names.py
@@ -5,12 +5,9 @@ while True:
     first = input("\nPlease give me a first name: ")
     if first == 'q':
         break
-    middle = input("Please give me a middle name: ")
-    if middle == 'q':
-        break
     last = input("Please give me a last name: ")
     if last == 'q':
         break
 
-    formatted_name = get_formatted_name(first, last, middle)
+    formatted_name = get_formatted_name(first, last)
     print("\tNeatly formatted name: " + formatted_name + '.')
